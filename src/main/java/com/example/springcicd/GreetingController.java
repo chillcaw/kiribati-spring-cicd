9c42c9743fe95bd0cb7ca92a763e63330f573e3e
Calum Hill-Cawthorne <calum@nology.io>
2022-05-19 10:02:27 +1000

    fixed syntax errors in greetingcontroller

--- src/main/java/com/example/springcicd/GreetingController.java
+++ src/main/java/com/example/springcicd/GreetingController.java
@@ -2,7 +2,7 @@ package com.example.springcicd;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;\
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 
